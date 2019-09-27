4403ea49e7bb9f2b9b5f2a7d8f4230d7cf659bd7
franco-ruggeri <fruggeri@kth.se>
2019-09-28 01:12:45 +0200

    score 25/25 TTT2D

--- TicTacToe2D/src/Player.java
+++ TicTacToe2D/src/Player.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Player {
+	private int whoAmI;
+	private static final int DEPTH = 3;
+	
     /**
      * Performs a move
      *
@@ -11,19 +14,143 @@ public class Player {
      * @return the next state the board is in after our move
      */
     public GameState play(final GameState gameState, final Deadline deadline) {
-        Vector<GameState> nextStates = new Vector<GameState>();
-        gameState.findPossibleMoves(nextStates);
+        whoAmI = gameState.getNextPlayer();
+        return alphabeta(gameState, DEPTH);
+    }
+    
+    private GameState alphabeta(GameState state, int depth) {
+    	Vector<GameState> nextStates = new Vector<>();
+    	int v = Integer.MIN_VALUE;
+    	GameState choice = null;
+    	
+    	// fill next states
+    	state.findPossibleMoves(nextStates);
 
-        if (nextStates.size() == 0) {
-            // Must play "pass" move if there are no other moves possible.
-            return new GameState(gameState, new Move());
-        }
+    	// invalid depth
+    	if (depth==0)
+    		return null;
+    	
+    	// no possible moves, pass
+    	if (nextStates.isEmpty())
+    		return new GameState(state, new Move());
 
-        /**
-         * Here you should write your algorithms to get the best next move, i.e.
-         * the best next state. This skeleton returns a random move instead.
-         */
-        Random random = new Random();
-        return nextStates.elementAt(random.nextInt(nextStates.size()));
-    }    
+    	// find action maximizing the "utility"
+    	for (GameState s : nextStates) {
+    		int tmp = alphabetaR(s, depth-1, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    		if (tmp > v) {
+    			v = tmp;		// max
+    			choice = s;		// argmax
+    		}
+    	}
+    	
+    	return choice;
+    }
+    
+    private int alphabetaR(GameState state, int depth, int alpha, int beta) {
+    	int player = state.getNextPlayer();
+    	Vector<GameState> nextStates = new Vector<>();
+    	int v;
+    	
+    	// fill next states
+    	state.findPossibleMoves(nextStates);
+    	
+    	// cutoff test
+    	if (depth == 0 || nextStates.isEmpty()) {
+    		v = evaluate(state);
+    	}
+    	// it's me, I look for the maximum
+    	else if (player == whoAmI) {
+    		v = Integer.MIN_VALUE;
+    		for (GameState s : nextStates) {
+    			int tmp = alphabetaR(s, depth-1, alpha, beta);
+    			if (tmp > v)
+    				v = tmp;
+    			if (tmp > alpha)
+    				alpha = tmp;
+    			if (beta <= alpha)
+    				break;
+    		}
+    	}
+    	// it's the opponent, he looks for the minimum
+    	else {
+    		v = Integer.MAX_VALUE;
+    		for (GameState s : nextStates) {
+    			int tmp = alphabetaR(s, depth-1, alpha, beta);
+    			if (tmp < v)
+    				v = tmp;
+    			if (tmp < beta)
+    				beta = tmp;
+    			if (beta <= alpha)
+    				break;
+    		}
+    	}
+    	
+    	return v;
+    }
+    
+    private class EvaluationData {
+    	int[] rows, cols, diags;
+    	
+    	EvaluationData() {
+    		rows = new int[GameState.BOARD_SIZE];
+    		cols = new int[GameState.BOARD_SIZE];
+        	diags = new int[2];
+        	Arrays.fill(rows, 0);
+        	Arrays.fill(cols, 0);
+        	Arrays.fill(diags, 0);
+    	}
+    	
+    	int sum() {
+    		return Arrays.stream(rows).sum() + Arrays.stream(cols).sum() + Arrays.stream(diags).sum();
+    	}
+    }
+    
+    private int evaluate(GameState state) {
+    	EvaluationData me = new EvaluationData();
+    	EvaluationData opponent = new EvaluationData();
+    	
+    	// terminal state, the result is certain
+    	if (state.isEOG()) {
+    		if (isWin(state))
+    			return Integer.MAX_VALUE;
+    		else if (isLoss(state))
+    			return Integer.MIN_VALUE;
+    		else
+    			return 0;	// draw
+    	}
+    	
+    	// estimate
+    	for (int i=0; i<GameState.CELL_COUNT; i++) {
+    		int mark = state.at(i);
+    		if (mark == Constants.CELL_EMPTY)
+    			continue;
+    		
+    		EvaluationData current = (mark==whoAmI ? me : opponent);
+    		EvaluationData other = (mark==whoAmI ? opponent : me);
+			int r = GameState.cellToRow(i);
+    		int c = GameState.cellToCol(i);
+    		current.rows[r]++;
+    		other.rows[r] = 0;	// clear, count only lines that could lead to victory
+    		current.cols[c]++;
+    		other.cols[c] = 0;
+    		if (r-c == 0) {		// main diagonal
+    			current.diags[0]++;
+    			other.diags[0] = 0;
+    		} else if (r+c == GameState.BOARD_SIZE-1) {	// anti diagonal
+    			current.diags[1]++;
+    			other.diags[1] = 0;
+    		}
+    	}
+    	return me.sum() - opponent.sum();
+    }
+    
+    private boolean isWin(GameState state) {
+    	return (whoAmI == Constants.CELL_X && state.isXWin()) ||
+    			(whoAmI == Constants.CELL_O && state.isOWin());
+    }
+    
+    private boolean isLoss(GameState state) {
+    	return (whoAmI == Constants.CELL_X && state.isOWin()) ||
+    			(whoAmI == Constants.CELL_O && state.isXWin());
+    }
 }
